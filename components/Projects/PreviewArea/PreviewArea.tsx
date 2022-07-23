53dc60f2e5404b5206f6ced91dc8f9087df70425
Jordan <spamadress94@hotmail.fr>
2022-07-23 14:42:30 +0900

    Slider animation + title underline animation

--- components/Projects/PreviewArea/PreviewArea.tsx
+++ components/Projects/PreviewArea/PreviewArea.tsx
@@ -1,10 +1,12 @@
 import { FunctionComponent } from 'react'
 import Image from 'next/image'
 
+import macbook from '../../../public/macbook.png'
 import dashboardPic from '../../../public/dashboard.png'
 import localhostPic from '../../../public/localhost.png'
 import phoneMockup from '../../../public/phone.png'
 import localhostPhone from '../../../public/localhost_3000_signup_phone.png'
+import habitsChrome from '../../../public/habitsChrome.png'
 
 import styles from '../Projects.module.css'
 
@@ -13,12 +15,18 @@ const PreviewArea: FunctionComponent = () => {
 		<div className={styles.inActionDiv}>
 			<h1>My past works :</h1>
 			<div className={styles.laptopDiv}>
+				<div className={styles.laptop}>
+					<Image src={macbook} alt="habits in action" layout="fill" />
+				</div>
 				<div className={styles.laptopDivMargin}>
 					<div className={`${styles.imgScreenshot} ${styles.img1}`}>
 						<Image src={dashboardPic} alt="habits in action" layout="fill" />
 					</div>
 					<div className={`${styles.imgScreenshot} ${styles.img2}`}>
 						<Image src={localhostPic} alt="habits in action" layout="fill" />
+					</div>
+					<div className={`${styles.imgScreenshot} ${styles.img3}`}>
+						<Image src={habitsChrome} alt="habits in action" layout="fill" />
 					</div>
 					<div className={`${styles.phoneMockup}`}>
 						<Image src={phoneMockup} alt="phone mockup" layout="fill" />
@@ -29,7 +37,15 @@ const PreviewArea: FunctionComponent = () => {
 					<div className={`${styles.imgScreenshotPhone} ${styles.img2}`}>
 						<Image src={localhostPhone} alt="habits in action" layout="fill" />
 					</div>
+					<div className={`${styles.imgScreenshotPhone} ${styles.img3}`}>
+						<Image src={habitsChrome} alt="habits in action" layout="fill" />
+					</div>
 				</div>
+			</div>
+
+			<div className={styles.sliderProjectNameDiv}>
+				<p className={styles.habitTitle}>Habits 30</p>
+				<p className={styles.TicketTitle}>Team Ticket</p>
 			</div>
 		</div>
 	)
