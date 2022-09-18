da3a29153261fccb8a6d61463215e62d2488c59f
Jordan <spamadress94@hotmail.fr>
2022-09-18 21:46:31 +0700

    Google analytics fix attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,19 @@ import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
+    {/* Bootstrap Script) */}
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" />
+
+    {/* Google tag (gtag.js) */}
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} strategy="afterInteractive"/>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+    
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`}
+    </Script>
+
     <Component {...pageProps} />
   </>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,18 +5,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-
-        {/* Google tag (gtag.js) */}
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} strategy="afterInteractive"/>
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`}
-        </Script>
-
-
         {/* boostrap */}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
 
