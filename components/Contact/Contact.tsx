318e25b5d670c00b06b3409a080aff111335af10
Jordan <spamadress94@hotmail.fr>
2022-09-24 15:41:25 +0800

    Add email contact info

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,98 +1,139 @@
-import { FunctionComponent, useRef, useState } from "react"
+import { FunctionComponent, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 
-import EnvelopeSVG from "../../public/envelope.svg"
+import EnvelopeSVG from '../../public/envelope.svg'
 
-import styles from "./Contact.module.css"
+import styles from './Contact.module.css'
 
 const CTA: FunctionComponent = () => {
-    const [formInputs, setFormInputs] = useState({
-        name: '',
-        email: '',
-        message: ''
-    })
-    const [isSending, setIsSending] = useState(false)
-    const [successMessage, setSuccessMessage] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
+	const [formInputs, setFormInputs] = useState({
+		name: '',
+		email: '',
+		message: '',
+	})
+	const [isSending, setIsSending] = useState(false)
+	const [successMessage, setSuccessMessage] = useState('')
+	const [errorMessage, setErrorMessage] = useState('')
 
-    const formRef = useRef(null)
+	const formRef = useRef(null)
 
-    function onChangeName(event: React.ChangeEvent<HTMLInputElement>) {
-        setFormInputs(prevInputs => ({...prevInputs, name: event.target.value}))
-    }
-    function onChangeEmail(event: React.ChangeEvent<HTMLInputElement>) {
-        setFormInputs(prevInputs => ({...prevInputs, email: event.target.value}))
-    }
-    function onChangeMessage(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        setFormInputs(prevInputs => ({...prevInputs, message: event.target.value}))
-    }
+	function onChangeName(event: React.ChangeEvent<HTMLInputElement>) {
+		setFormInputs((prevInputs) => ({ ...prevInputs, name: event.target.value }))
+	}
+	function onChangeEmail(event: React.ChangeEvent<HTMLInputElement>) {
+		setFormInputs((prevInputs) => ({ ...prevInputs, email: event.target.value }))
+	}
+	function onChangeMessage(event: React.ChangeEvent<HTMLTextAreaElement>) {
+		setFormInputs((prevInputs) => ({ ...prevInputs, message: event.target.value }))
+	}
 
-    async function onSubmit(event: React.FormEvent){
-        event.preventDefault()
-        setIsSending(true)
-        setSuccessMessage('')
-        setErrorMessage('')
-        await sendEmail()
-        setIsSending(false)
-    }
+	async function onSubmit(event: React.FormEvent) {
+		event.preventDefault()
+		setIsSending(true)
+		setSuccessMessage('')
+		setErrorMessage('')
+		await sendEmail()
+		setIsSending(false)
+	}
 
-    async function sendEmail() {
-        if(!process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID || !process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID || !process.env.NEXT_PUBLIC_EMAIL_APIKEY || !formRef.current){
-            setErrorMessage("Something went wrong. Please try again.")
-            return
-        }
-        try {
-            await emailjs.sendForm(process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, formRef.current, process.env.NEXT_PUBLIC_EMAIL_APIKEY)
-            setSuccessMessage("Thank you. Your message has been sent successfully!")
-            setFormInputs({
-                name: '',
-                email: '',
-                message: ''
-            })
-        } catch (error: any) {
-            setErrorMessage("Something went wrong. Please try again.")
-            console.log(error.text)
-        }
+	async function sendEmail() {
+		if (
+			!process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID ||
+			!process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID ||
+			!process.env.NEXT_PUBLIC_EMAIL_APIKEY ||
+			!formRef.current
+		) {
+			setErrorMessage('Something went wrong. Please try again.')
+			return
+		}
+		try {
+			await emailjs.sendForm(
+				process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+				process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
+				formRef.current,
+				process.env.NEXT_PUBLIC_EMAIL_APIKEY
+			)
+			setSuccessMessage('Thank you. Your message has been sent successfully!')
+			setFormInputs({
+				name: '',
+				email: '',
+				message: '',
+			})
+		} catch (error: any) {
+			setErrorMessage('Something went wrong. Please try again.')
+			console.log(error.text)
+		}
+	}
 
-    }
-
-    return(
-            <section id="contact" className={styles.section}>
-                <div className="my-container">
-                    <div className="container-text-align">
-                        <h3 className="section-title">Contact me</h3>
-                    </div>
-                    <div className={styles.formContentContainer}>
-                            <form ref={formRef} className="contact-form" onSubmit={onSubmit}>
-                                
-                                <div className={styles.maiLAndEmailDiv}>
-                                    <div className={styles.flexColumn}>
-                                        <label className={styles.label} htmlFor="user_name">Name</label>
-                                        <input required className={styles.input} name="user_name" id="user_name" type="text" value={formInputs.name} onChange={onChangeName}/>
-                                    </div>
-                                    
-                                    <div className={styles.flexColumn}>
-                                        <label className={styles.label} htmlFor="user_email">Email</label>
-                                        <input required className={styles.input} name="user_email" id="user_email" type="email" value={formInputs.email} onChange={onChangeEmail}/>
-                                    </div>
+	return (
+		<section id="contact" className={styles.section}>
+			<div className="my-container">
+				<div className="container-text-align">
+					<h3 className="section-title">Contact me</h3>
+				</div>
+				<div className={styles.formContentContainer}>
+                    <div>
+                        <form ref={formRef} className="contact-form" onSubmit={onSubmit}>
+                            <div className={styles.maiLAndEmailDiv}>
+                                <div className={styles.flexColumn}>
+                                    <label className={styles.label} htmlFor="user_name">
+                                        Name
+                                    </label>
+                                    <input
+                                        required
+                                        className={styles.input}
+                                        name="user_name"
+                                        id="user_name"
+                                        type="text"
+                                        value={formInputs.name}
+                                        onChange={onChangeName}
+                                    />
                                 </div>
-                                
-                                <label className={styles.label} htmlFor="message">Message</label>
-                                <textarea required className={styles.textarea} name="message" id="message" value={formInputs.message} onChange={onChangeMessage}></textarea>
-                                
-                                <button disabled={isSending} className="my-btn btn-form">{isSending ? 'Sending...' : 'Send message'}</button>
-                                
-                                {successMessage && <p className={`${styles.formConfirmation} ${styles.success}`}>{successMessage}</p>}
-                                {errorMessage && <p className={`${styles.formConfirmation} ${styles.error}`}>{errorMessage}</p>}
 
-                            </form>
-                            <div className={styles.imageContainer}>
-                                <EnvelopeSVG className={styles.illustrationSvg} />
+                                <div className={styles.flexColumn}>
+                                    <label className={styles.label} htmlFor="user_email">
+                                        Email
+                                    </label>
+                                    <input
+                                        required
+                                        className={styles.input}
+                                        name="user_email"
+                                        id="user_email"
+                                        type="email"
+                                        value={formInputs.email}
+                                        onChange={onChangeEmail}
+                                    />
+                                </div>
                             </div>
+
+                            <label className={styles.label} htmlFor="message">
+                                Message
+                            </label>
+                            <textarea
+                                required
+                                className={styles.textarea}
+                                name="message"
+                                id="message"
+                                value={formInputs.message}
+                                onChange={onChangeMessage}
+                            ></textarea>
+
+                            <button disabled={isSending} className="my-btn btn-form">
+                                {isSending ? 'Sending...' : 'Send message'}
+                            </button>
+
+                            {successMessage && <p className={`${styles.formConfirmation} ${styles.success}`}>{successMessage}</p>}
+                            {errorMessage && <p className={`${styles.formConfirmation} ${styles.error}`}>{errorMessage}</p>}
+                        </form>
+                        <p className={styles.reachmeParagraph}>You can also reach me directly at : jordan.acker.dev@gmail.com</p>
                     </div>
-                </div>
-            </section>
-    )
+					<div className={styles.imageContainer}>
+						<EnvelopeSVG className={styles.illustrationSvg} />
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default CTA
--- improvements.md
+++ improvements.md
@@ -1,14 +1,12 @@
-- Put my email as well in the contact section ?
-- Reduce number of screenshots / projects in preview to 2 or 1 ?
+- Put my email as well in the contact section !
 - Change Hero ? Put cool moving SVG / illustration on right side instead ?
-- Add captiosn to carousel screenshot ? try
+- Improve screenshot habits landing phone ?
+- Add captions to carousel screenshot ? try
 
 - Mettre subtitle des projects un peu plus flashy ?
-- Improve screenshot habits landing phone ?
 - Learn + add Jest as skill
 
-- Learn + add dockers ?
-- Learn and add skills Sass (& tailwind)
+- Learn and add skills Tailwind (& sass mayyybe)
 - Learn + add Docker as a skill
 - upgrade background : try background-image: linear-gradient(rgba(255, 255, 255, 0), rgb(247, 250, 255) 100%); + SVG
 : and try background-color: rgb(227,242,253);
